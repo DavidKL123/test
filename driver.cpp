60dc429025364d1a1ec0fb0e0ea8b1f9488d2bd2
elimor123 <elimo2@ac.sce.ac.il>
2018-11-24 23:09:07 +0200

    i am eli and i recently did a change.

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include <windows.h>
 using namespace std;
 
 int main() {
 	cout << "hello world!" << endl;
+	Sleep(2000);
+	cout << "This is eli in the mood~" << endl;
+	system("PAUSE");
 	return 0;
 }
